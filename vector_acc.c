b879df273def6348f39c2e3c8c1c395a10f92909
maoruichao <33387746+maoruichao@users.noreply.github.com>
2017-12-17 20:30:20 +0800

    Create vector_acc.c

---
+++ vector_acc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+//reduction
+int main()
+{
+    int i, n;
+    n=1000;
+    float a[n], b[n], result;
+    // initializations
+    //n = 100;
+    result = 0.0;
+    for (i = 0; i < n; i++)
+    {
+        a[i] = i * 1.0;
+        b[i] = i * 2.0;
+        
+    }
+    #pragma acc parallel for private(i) reduction(+:result)
+    for (i = 0; i < n; i++)
+    {
+        
+        result = result + (a[i] * b[i]);
+    }
+    
+    printf("a*b= %12.6lf\n", result);
+    
+    return 0;
+}
